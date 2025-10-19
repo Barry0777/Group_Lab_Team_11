3ec155920e84c44d5c4d023f7418eaf08d5d6389
JackChotipat <metreethumaporn.c@northeastern.edu>
2025-10-18 21:11:32 -0400

    Create Main.java

---
+++ TeamGreeting/src/Model/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author User PC
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Chotipat.main(args);
+    }
+}
